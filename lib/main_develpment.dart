ed127f79d3b7986f177d79681de6d7818faa0902
Ahmed khaled <ahmed49876@gmail.com>
2024-05-15 15:35:09 +0300

    - added Development & Productions flavors for Andriod. - chang  the  Andriod app name for each flavor. - added launch.json for flavors config. -added two main files for each flavor.

--- lib/main_develpment.dart
+++ lib/main_develpment.dart
@@ -5,11 +5,10 @@ import 'package:graduation_project_2024/Core/routing/app_router.dart';
 import 'package:graduation_project_2024/pharmacy_app.dart';
 
 
-void maim() {
-  //setupGetIt();
+void maim()async {
+  setupGetIt();
   // To fix texts being hidden bug in flutter_screenutil in release mode.
-  // ScreenUtil.ensureScreenSize();
-  //setupGetIt();
+ await ScreenUtil.ensureScreenSize();
   runApp(NetPharmacyApp(
     appRouter: AppRouter(),
   ),
---
+++ lib/main_production.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:graduation_project_2024/Core/di/dependency_injection.dart';
+import 'package:graduation_project_2024/Core/routing/app_router.dart';
+import 'package:graduation_project_2024/pharmacy_app.dart';
+
+
+void maim() {
+  setupGetIt();
+  // To fix texts being hidden bug in flutter_screenutil in release mode.
+  ScreenUtil.ensureScreenSize();
+  runApp(NetPharmacyApp(
+    appRouter: AppRouter(),
+  ),
+  );
+}
